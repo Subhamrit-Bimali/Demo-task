fd452b89476c845eedb495d7b1770800348f0255
SubhamritBimali <sbimali@deerhold.org>
2022-11-16 13:35:57 +0545

    final changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { json } from "body-parser";
 import { router } from "./routes";
+import { config } from "./helper/config";
 require("./helper/db-connect");
 const app = express();
 app.use(json());
 
 app.use(router);
 
-app.listen(4000, () => {
-  console.log("App running on port 4000");
+app.listen(config.server.port, () => {
+  console.log(`App running on port ${config.server.port}`);
 });
